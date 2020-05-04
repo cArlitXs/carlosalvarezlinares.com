b61fc976736dd9ee006f4ab94bff0a8e24555c78
carlitosalli <carlitosalvarezlinares@gmail.com>
2020-05-04 12:07:10 +0200

    Add loader

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,12 +1,35 @@
 'use strict';
 
-// const path = 'http://localhost:5500';
-const path = 'https://carlosalvarezlinares.com';
+window.addEventListener('scroll', noScroll);
+
+window.addEventListener('load', (event) => {
+    loader();
+    window.removeEventListener('scroll', noScroll);
+});
+
+function noScroll() {
+    window.scrollTo(0, 0);
+}
 
 // ON LOAD
 document.addEventListener('DOMContentLoaded', function () {
-    particlesJS.load('particles-js', '/src/js/particlesjs-config.json', () => consoleHello());
+    particlesJS.load('particles-js', '/src/js/particlesjs-config.json', () => (consoleHello()));
 });
+
+// LOADER
+function loader() {
+    let loader = document.querySelector('#loader');
+    let back = document.querySelector('#back');
+    let body = document.body;
+
+    loader.style.visibility = 'hidden';
+    loader.style.transition = "all 0.5s";
+    loader.style.opacity = '0';
+    back.style.visibility = 'hidden';
+    back.style.transition = "all 0.5s";
+    back.style.opacity = '0';
+}
+
 
 // SHOW MODAL DAW
 function showModalDaw(e) {
@@ -62,3 +85,44 @@ function closeNavbar(e) {
 // SCROLL REVEAL
 ScrollReveal({ reset: true }).reveal('.scrollreveal');
 
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
